49723454bac747531b55d674b79110d4b987a4e8
quanNMHE170597 <117750627+quanNMHE170597@users.noreply.github.com>
2025-06-10 23:51:44 +0700

    basic authentication login

---
+++ hrms-backend/src/main/java/sep490/com/example/hrms_backend/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package sep490.com.example.hrms_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
+
--- hrms-backend/src/main/java/sep490/com/example/hrms_backend/service/impl/AuthServiceImpl.java
+++ hrms-backend/src/main/java/sep490/com/example/hrms_backend/service/impl/AuthServiceImpl.java
@@ -8,7 +8,7 @@ import sep490.com.example.hrms_backend.service.AuthService;
 @Service
 @AllArgsConstructor
 public class AuthServiceImpl implements AuthService {
-    private AuthenticationManager authenticationManager;
+//    private AuthenticationManager authenticationManager;
     @Override
     public String login(LoginDto loginDto) {
         return null;
