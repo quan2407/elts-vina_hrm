e72917c78ea84ef22e5440bbee10b80f6945eeeb
quanNMHE170597 <117750627+quanNMHE170597@users.noreply.github.com>
2025-06-11 00:59:03 +0700

    add AccountRepository and RoleRepository

---
+++ hrms-backend/src/main/java/sep490/com/example/hrms_backend/repository/AccountRepository.java
@@ -0,0 +1,18 @@
+package sep490.com.example.hrms_backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sep490.com.example.hrms_backend.entity.Account;
+
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByEmail(String email);
+
+    Optional<Account> findByUsernameOrEmail(String username, String email);
+
+    Optional<Account> findByUsername(String username);
+
+    Boolean exitsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
