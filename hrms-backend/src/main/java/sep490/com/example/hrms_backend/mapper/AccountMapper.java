96a4c521de4605c6e27e75078aa2fcee8a3e0fc0
quanNMHE170597 <117750627+quanNMHE170597@users.noreply.github.com>
2025-06-12 15:54:18 +0700

    (account) get account for admin api backend

---
+++ hrms-backend/src/main/java/sep490/com/example/hrms_backend/mapper/AccountMapper.java
@@ -0,0 +1,28 @@
+package sep490.com.example.hrms_backend.mapper;
+
+import sep490.com.example.hrms_backend.entity.Account;
+import sep490.com.example.hrms_backend.dto.response.AccountResponseDTO;
+
+import java.util.stream.Collectors;
+
+public class AccountMapper {
+
+    public static AccountResponseDTO mapToAccountResponseDTO(Account account, AccountResponseDTO dto) {
+        dto.setAccountId(account.getAccountId());
+        dto.setUsername(account.getUsername());
+        dto.setEmail(account.getEmail());
+        dto.setIsActive(account.getIsActive());
+        dto.setLastLoginAt(account.getLastLoginAt());
+
+        if (account.getRoles() != null) {
+            dto.setRoles(
+                    account.getRoles().stream()
+                            .map(role -> role.getRoleName())
+                            .collect(Collectors.toSet())
+            );
+        }
+
+        return dto;
+    }
+
+}
