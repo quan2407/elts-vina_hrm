d8ed542af2b97037e42044a4e1e03aa324fdd165
TuNN <tus.nngoc@gmail.com>
2025-06-17 21:18:19 +0700

    merge with new structure front end

--- hrms-backend/src/main/java/sep490/com/example/hrms_backend/controller/BenefitRegistrationController.java
+++ hrms-backend/src/main/java/sep490/com/example/hrms_backend/controller/BenefitRegistrationController.java
@@ -3,13 +3,14 @@ package sep490.com.example.hrms_backend.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sep490.com.example.hrms_backend.service.BenefitRegistrationService;
 
 @RestController
 @RequestMapping("api/benefit")
 public class BenefitRegistrationController {
 
     @Autowired
-    private BenefitRegistrationController benefitRegistrationController;
+    private BenefitRegistrationService benefitRegistrationService;
 
 
 }
