53d80ed8143c3e1cf0c5e65e6255b6e12038f015
quanNMHE170597 <117750627+quanNMHE170597@users.noreply.github.com>
2025-06-18 13:33:25 +0700

    (backend) add line repository

---
+++ hrms-backend/src/main/java/sep490/com/example/hrms_backend/repository/LineRepository.java
@@ -0,0 +1,7 @@
+package sep490.com.example.hrms_backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sep490.com.example.hrms_backend.entity.Line;
+
+public interface LineRepository extends JpaRepository<Line,Long> {
+}
