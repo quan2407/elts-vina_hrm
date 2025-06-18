ad68d91270be8c4c90d1bf7751290557d0ef2613
quanNMHE170597 <117750627+quanNMHE170597@users.noreply.github.com>
2025-06-18 13:38:30 +0700

    (backend) add mapper from Request to entity

--- hrms-backend/src/main/java/sep490/com/example/hrms_backend/mapper/EmployeeMapper.java
+++ hrms-backend/src/main/java/sep490/com/example/hrms_backend/mapper/EmployeeMapper.java
@@ -1,5 +1,6 @@
 package sep490.com.example.hrms_backend.mapper;
 
+import sep490.com.example.hrms_backend.dto.EmployeeRequestDTO;
 import sep490.com.example.hrms_backend.dto.EmployeeResponseDTO;
 import sep490.com.example.hrms_backend.entity.Employee;
 
@@ -25,4 +26,25 @@ public class EmployeeMapper {
                 .accountUsername(employee.getAccount() != null ? employee.getAccount().getUsername() : null)
                 .build();
     }
+    public static Employee mapToEmployee(EmployeeRequestDTO dto) {
+        return Employee.builder()
+                .employeeCode(dto.getEmployeeCode())
+                .employeeName(dto.getEmployeeName())
+                .gender(dto.getGender() != null ? dto.getGender().name() : null)
+                .dob(dto.getDob())
+                .placeOfBirth(dto.getPlaceOfBirth())
+                .originPlace(dto.getOriginPlace())
+                .nationality(dto.getNationality())
+                .citizenId(dto.getCitizenId())
+                .citizenIssueDate(dto.getCitizenIssueDate())
+                .citizenExpiryDate(dto.getCitizenExpiryDate())
+                .citizenIssuePlace(dto.getCitizenIssuePlace())
+                .address(dto.getAddress())
+                .image(dto.getImage())
+                .startWorkAt(dto.getStartWorkAt())
+                .phoneNumber(dto.getPhoneNumber())
+                .email(dto.getEmail())
+                .build();
+    }
+
 }
--- hrms-backend/src/main/java/sep490/com/example/hrms_backend/service/impl/EmployeeServiceImpl.java
+++ hrms-backend/src/main/java/sep490/com/example/hrms_backend/service/impl/EmployeeServiceImpl.java
@@ -1,11 +1,19 @@
 package sep490.com.example.hrms_backend.service.impl;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import sep490.com.example.hrms_backend.dto.EmployeeRequestDTO;
 import sep490.com.example.hrms_backend.dto.EmployeeResponseDTO;
+import sep490.com.example.hrms_backend.entity.Department;
+import sep490.com.example.hrms_backend.entity.Employee;
+import sep490.com.example.hrms_backend.entity.Line;
+import sep490.com.example.hrms_backend.entity.Position;
 import sep490.com.example.hrms_backend.mapper.EmployeeMapper;
+import sep490.com.example.hrms_backend.repository.DepartmentRepository;
 import sep490.com.example.hrms_backend.repository.EmployeeRepository;
+import sep490.com.example.hrms_backend.repository.LineRepository;
+import sep490.com.example.hrms_backend.repository.PositionRepository;
 import sep490.com.example.hrms_backend.service.EmployeeService;
 
 import java.util.List;
@@ -16,6 +24,9 @@ import java.util.stream.Collectors;
 public class EmployeeServiceImpl implements EmployeeService {
 
     private final EmployeeRepository employeeRepository;
+    private final DepartmentRepository departmentRepository;
+    private final LineRepository lineRepository;
+    private final PositionRepository positionRepository;
 
     @Override
     public List<EmployeeResponseDTO> getAllEmployees() {
@@ -25,8 +36,30 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     @Override
+    @Transactional
     public EmployeeResponseDTO createEmployee(EmployeeRequestDTO dto) {
-        return null;
+        Employee employee = EmployeeMapper.mapToEmployee(dto);
+        employee.setDepartment(fetchDepartment(dto.getDepartmentId()));
+        employee.setPosition(fetchPosition(dto.getPositionId()));
+        employee.setLine(fetchLine(dto.getLineId()));
+        employee = employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeResponseDTO(employee);
     }
+
+    private Department fetchDepartment(Long id) {
+        return departmentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Department not found with ID: " + id));
+    }
+
+    private Position fetchPosition(Long id) {
+        return positionRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Position not found with ID: " + id));
+    }
+
+    private Line fetchLine(Long id) {
+        return lineRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Line not found with ID: " + id));
+    }
+
 
 }
