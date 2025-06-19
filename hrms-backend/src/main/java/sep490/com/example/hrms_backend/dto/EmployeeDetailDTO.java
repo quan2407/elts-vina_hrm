9cdb526536e74ae98cb351e4fd834c14e11d8049
quanNMHE170597 <117750627+quanNMHE170597@users.noreply.github.com>
2025-06-19 16:00:05 +0700

    (backend) add employee detail dto

---
+++ hrms-backend/src/main/java/sep490/com/example/hrms_backend/dto/EmployeeDetailDTO.java
@@ -0,0 +1,35 @@
+package sep490.com.example.hrms_backend.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EmployeeDetailDTO {
+    private Long employeeId;
+    private String employeeCode;
+    private String employeeName;
+    private String gender;
+    private LocalDate dob;
+    private String placeOfBirth;
+    private String originPlace;
+    private String nationality;
+    private String citizenId;
+    private LocalDate citizenIssueDate;
+    private LocalDate citizenExpiryDate;
+    private String citizenIssuePlace;
+    private String address;
+    private String image;
+    private LocalDate startWorkAt;
+    private String phoneNumber;
+    private String email;
+
+    private String departmentName;
+    private String positionName;
+    private String lineName;
+    private String accountUsername;
+}
