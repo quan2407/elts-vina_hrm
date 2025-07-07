36b2f105e621cb190660aaf16d0c3dce680af728
quanNMHE170597 <117750627+quanNMHE170597@users.noreply.github.com>
2025-07-07 17:42:18 +0700

    (back) add checkin checkout

--- hrms-backend/src/main/java/sep490/com/example/hrms_backend/dto/AttendanceCellDTO.java
+++ hrms-backend/src/main/java/sep490/com/example/hrms_backend/dto/AttendanceCellDTO.java
@@ -8,8 +8,17 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class AttendanceCellDTO {
+    private Long attendanceRecordId;
     private String shift;
     private String overtime;
     private String weekend;
     private String holiday;
+    private boolean hasScheduleDetail;
+    private String checkIn;
+    private String checkOut;
+    private boolean holidayFlag;
+    private boolean weekendFlag;
+
+
+
 }
