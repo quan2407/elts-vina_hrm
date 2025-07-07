dae57a6197eb928776b951895a2388a0720dfcb9
quanNMHE170597 <117750627+quanNMHE170597@users.noreply.github.com>
2025-07-07 17:44:54 +0700

    (database) update database about holiday

--- sample_data/sample_data.sql
+++ sample_data/sample_data.sql
@@ -15,6 +15,7 @@ DELETE FROM interview_schedule;
 DELETE FROM candidate_recruitment;
 DELETE FROM candidate;
 DELETE FROM recruitment;
+DELETE FROM holidays;
 
 DELETE FROM `lines`;
 DELETE FROM account;
@@ -40,6 +41,7 @@ ALTER TABLE employee AUTO_INCREMENT = 1;
 ALTER TABLE position AUTO_INCREMENT = 1;
 ALTER TABLE department AUTO_INCREMENT = 1;
 ALTER TABLE role AUTO_INCREMENT = 1;
+ALTER TABLE holidays AUTO_INCREMENT = 1;
 
 
 INSERT INTO role (role_id, role_name) VALUES
@@ -200,12 +202,8 @@ INSERT INTO `lines` (line_id, line_name, department_id, leader_id) VALUES
 
 
 
-UPDATE employee SET line_id = 1 WHERE employee_id = 1; 
-UPDATE employee SET line_id = 2 WHERE employee_id = 2;  
-UPDATE employee SET line_id = 6 WHERE employee_id = 3;  
-UPDATE employee SET line_id = 1 WHERE employee_id = 4; 
+
 UPDATE employee SET line_id = 4 WHERE employee_id = 6; 
-UPDATE employee SET line_id = 6 WHERE employee_id = 7;  
 UPDATE employee SET line_id = 4 WHERE employee_id = 8;  
 UPDATE employee SET line_id = 4 WHERE employee_id = 9; 
 UPDATE employee SET line_id = 4 WHERE employee_id = 10; 
@@ -270,3 +268,9 @@ INSERT INTO benefit(title, description, end_date, is_active, max_participants, s
 ('Du lịch công ty 2025', 'Chuyến du lịch thường niên cùng công ty đến Đà Nẵng trong 3 ngày 2 đêm.', '2025-08-15', 0, 100, '2025-06-28'),
 ('Gói hỗ trợ sức khỏe tinh thần', 'Miễn phí 5 buổi tư vấn tâm lý cùng chuyên gia.', '2026-01-15', 1, 300, '2025-07-01'),
 ('Phụ cấp thể thao', 'Hỗ trợ chi phí tham gia phòng gym, yoga, hoặc các hoạt động thể thao.', '2025-11-01', 1, 5, '2025-06-28');
+
+INSERT INTO holidays (date, name, is_recurring) VALUES
+('2025-01-01', 'Tết dương lịch', true),
+('2025-04-30', 'Giải phóng miền Nam', true),
+('2025-05-01', 'Quốc tế Lao động', true),
+('2025-09-02', 'Quốc khánh', true);
