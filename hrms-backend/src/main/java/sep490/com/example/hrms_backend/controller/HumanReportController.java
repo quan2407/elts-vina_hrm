0c12cbd77b831b382b0fdc2d755a95055456cb00
Tran Tung <transontungpl03@gmail.com>
2025-07-26 02:40:33 +0700

    update export bao cao nhan luc

--- hrms-backend/src/main/java/sep490/com/example/hrms_backend/controller/HumanReportController.java
+++ hrms-backend/src/main/java/sep490/com/example/hrms_backend/controller/HumanReportController.java
@@ -2,7 +2,11 @@ package sep490.com.example.hrms_backend.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +16,7 @@ import sep490.com.example.hrms_backend.dto.AttendanceMonthlyViewDTO;
 import sep490.com.example.hrms_backend.dto.EmployeeResponseDTO;
 import sep490.com.example.hrms_backend.service.HumanReportService;
 
+import java.io.ByteArrayInputStream;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
@@ -44,6 +49,18 @@ public class HumanReportController {
 
         Map<String, List<AttendanceMonthlyViewDTO>> humanAbsent = humanReportService.getListEmpAbsentKL(date);
         return humanAbsent;
+    }
+    @GetMapping("/export")
+    @PreAuthorize("hasAnyRole('ADMIN', 'HR')")
+    public ResponseEntity<InputStreamResource> exportHumanReportToExcel(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+        ByteArrayInputStream in = humanReportService.exportHumanReportToExcel(date);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "attachment; filename=baocaonhanluc.xlsx");
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentType(MediaType.parseMediaType(
+                        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"))
+                .body(new InputStreamResource(in));
     }
 
 }
