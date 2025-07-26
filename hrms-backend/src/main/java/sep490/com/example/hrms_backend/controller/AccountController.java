1ed589194bfbb0182f6218c8915a3f23bd146208
quanNMHE170597 <117750627+quanNMHE170597@users.noreply.github.com>
2025-07-26 14:07:30 +0700

    (backend) delete pre authorize

--- hrms-backend/src/main/java/sep490/com/example/hrms_backend/controller/AccountController.java
+++ hrms-backend/src/main/java/sep490/com/example/hrms_backend/controller/AccountController.java
@@ -2,7 +2,6 @@ package sep490.com.example.hrms_backend.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import sep490.com.example.hrms_backend.dto.AccountResponseDTO;
 import sep490.com.example.hrms_backend.service.AccountService;
@@ -16,18 +15,15 @@ public class AccountController {
 
     private final AccountService accountService;
 
-
     @GetMapping
-    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<AccountResponseDTO>> getAllAccounts() {
         List<AccountResponseDTO> accountList = accountService.getAllAccounts();
         return ResponseEntity.ok(accountList);
     }
+
     @PutMapping("/{id}/toggle-status")
-    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> toggleAccountStatus(@PathVariable Long id) {
         accountService.toggleAccountStatus(id);
         return ResponseEntity.ok().build();
     }
-
 }
