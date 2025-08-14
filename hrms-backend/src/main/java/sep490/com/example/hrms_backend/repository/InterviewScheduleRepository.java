05ec30330504d3b7a05a99ce0f0fea36f230e113
Tran Tung <transontungpl03@gmail.com>
2025-08-14 17:27:42 +0700

    add notifcation

--- hrms-backend/src/main/java/sep490/com/example/hrms_backend/repository/InterviewScheduleRepository.java
+++ hrms-backend/src/main/java/sep490/com/example/hrms_backend/repository/InterviewScheduleRepository.java
@@ -4,5 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import sep490.com.example.hrms_backend.entity.InterviewSchedule;
 
+import java.util.List;
+
 public interface InterviewScheduleRepository extends JpaRepository<InterviewSchedule, Long> {
+
+    List<InterviewSchedule> findAllByInterviewer_EmployeeId(Long employeeId);
 }
