b9c69e57b1ccde53e045a652f6af143b8b8b05e2
Tu Travis <tus.nngoc@gmail.com>
2025-08-15 15:50:06 +0700

    BE: add application.yml, CORS (and SecurityConfig)

---
+++ hrms-backend/src/main/java/sep490/com/example/hrms_backend/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package sep490.com.example.hrms_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOriginPatterns("http://localhost:*", "https://*.vercel.app")
+                .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
